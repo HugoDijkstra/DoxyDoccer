011b2f34c8f1ac0e55d8adc6624bf7825d443eeb
Hugo Dijkstra <hugodijkstra@live.nl>
2017-02-03 15:43:40 +0100

    completed reading files

--- main.cpp
+++ main.cpp
@@ -7,14 +7,24 @@
 void showHelp();
 void documentFile(std::string path);
 void documentAllFiles();
-bool checkIfVectorContainsWord(std::vector<std::string> v, std::string word);
+void addBannedWords();
+void addBannedParts();
 
+bool checkIfVectorContainsWord(std::vector<std::string> v, std::string word);
+bool checkIfWordContainsPart(std::string word, std::string part);
+bool checkIfContainsPartOfWordInVector(std::vector<std::string> v,
+                                       std::string word);
 std::string ss;
 
 std::vector<std::string> variables;
 std::vector<std::string> functions;
+std::vector<std::string> bannedWords;
+std::vector<std::string> bannedParts;
 
 int main(int argc, char const *argv[]) {
+
+  addBannedWords();
+  addBannedParts();
   if (argc > 1)
     ss = argv[1];
   else {
@@ -54,7 +64,8 @@ void documentFile(std::string path) {
       std::string word = "";
 
       std::string parameters;
-      int wordCount = 0;
+      int wordCount;
+      wordCount = 1;
       std::string::iterator it = line.begin();
       while (it != line.end()) {
         if ((*it) == '(') {
@@ -65,26 +76,38 @@ void documentFile(std::string path) {
           continue;
         } else if ((*it) == ')') {
           cagedDepht--;
-          word += parameters;
+          if (checkIfWordContainsPart(parameters, " ") ||
+              parameters.length() == 0)
+            word += parameters;
+          else
+            break;
           word += (*it);
           it++;
           continue;
         }
         if (cagedDepht == 0) {
-          if ((*it) == ' ') {
-            if (lastWord[0] != ' ' || (int)lastWord[0] != 0) {
+          if ((int)(*it) == 32) {
+            if ((int)lastWord[0] != 0) {
               wordCount++;
             }
             // std::cout << word << std::endl;
+
             lastWord = word;
             word = "";
           } else if ((*it) == ';') {
             wordCount++;
             std::cout << wordCount << " : " << line << std::endl;
             if (wordCount == 2)
-              if (!checkIfVectorContainsWord(variables, word)) {
-
-                variables.push_back(word);
+              if (!checkIfVectorContainsWord(variables, word) &&
+                  !checkIfVectorContainsWord(functions, word) &&
+                  !checkIfVectorContainsWord(bannedWords, lastWord) &&
+                  !checkIfVectorContainsWord(bannedWords, word) &&
+                  !checkIfContainsPartOfWordInVector(bannedParts, word) &&
+                  !checkIfContainsPartOfWordInVector(bannedParts, lastWord)) {
+                if (checkIfWordContainsPart(word, "(")) {
+                  functions.push_back(word);
+                } else
+                  variables.push_back(word);
               }
             break;
           } else
@@ -95,10 +118,18 @@ void documentFile(std::string path) {
         it++;
       }
     }
+    std::cout << "\nVariables:" << std::endl;
     std::vector<std::string>::iterator it = variables.begin();
     while (it != variables.end()) {
       std::cout << (*it) << std::endl;
       it++;
+    }
+
+    std::cout << "\nFunctions:" << std::endl;
+    std::vector<std::string>::iterator jt = functions.begin();
+    while (jt != functions.end()) {
+      std::cout << (*jt) << std::endl;
+      jt++;
     }
 
   } else {
@@ -117,5 +148,44 @@ bool checkIfVectorContainsWord(std::vector<std::string> v, std::string word) {
   }
   return false;
 }
-bool checkIfWordContainsPart(std::string word, std::string part) {}
+bool checkIfWordContainsPart(std::string word, std::string part) {
+  std::string::iterator it = word.begin();
+  int count = 0;
+  while (it != word.end()) {
+    if ((*it) == part[0]) {
+      std::string str = "";
+      for (int i = 0; i < part.length(); i++) {
+        str += word[count + i];
+      }
+      if (str == part)
+        return true;
+    }
+    count++;
+    it++;
+  }
+  return false;
+}
+
+bool checkIfContainsPartOfWordInVector(std::vector<std::string> v,
+                                       std::string word) {
+  std::vector<std::string>::iterator it = v.begin();
+  while (it != v.end()) {
+    if (checkIfWordContainsPart(word, (*it))) {
+      return true;
+    }
+    it++;
+  }
+  return false;
+}
+void addBannedWords() {
+  bannedWords.push_back("return");
+  bannedWords.push_back("break");
+  bannedWords.push_back("continue");
+}
+void addBannedParts() {
+  bannedParts.push_back(".");
+  bannedParts.push_back("++");
+  bannedParts.push_back("--");
+}
+
 void documentAllFiles() { std::cout << "Documenting all files" << std::endl; }
